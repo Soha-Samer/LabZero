0044ae19e35110fe2f738af2c75fab4884ef057e
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2022-09-26 13:24:05 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[]array=new int[]{1,2,3,4,5,8,11,14,30};
+        String[]strarray=new String[]{"Omar","Zeyad","Karim","Ahmed"};
+        //get the average of integer array
+
+        //get largest and smallest value of integer array
+
+        //Reverse the string array
+
+        //sort the both string and integer array
+    }
+}
